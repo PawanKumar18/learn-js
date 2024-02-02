9c169afa9b7ab68abb894f25bc8588159ab9b424
Pawan Kumar <naveen.p@northeastern.edu>
2024-02-02 17:57:32 -0500

    Activity (Jan 30)

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,14 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+
+function logMsg() {
+  if (this && this.errMsg) {
+    console.error(this.errMsg);
+  }
+}
+
+const test = {
+  activity: "Activity (Jan 30)",
+  errMsg: "Error message test",
+};
+
+logMsg.apply(test);
